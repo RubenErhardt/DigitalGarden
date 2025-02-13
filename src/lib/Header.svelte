61927d9ef82f32cc13b7421c42b1f33fa46474f3
RubenErhardt <rubengamebattles@outlook.com>
2025-02-13 14:55:45 +0100

    Sveltekit install + begin header

---
+++ src/lib/Header.svelte
@@ -0,0 +1,34 @@
+<section class="Header">
+    <img src="/LogoHeader.png" alt="Header Logo" />
+    <div class="Right-side">
+        <ul class="Nav-right">
+            <li>About</li>
+            <li>Github</li>
+        </ul>
+        <button class="Email">
+            Mail me
+        </button>
+    </div>
+</section>
+
+<style>
+    .Header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 2rem;
+    }
+
+    .Right-side {
+        display: flex;
+        background-color: #f0f0f0;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem;
+    }
+
+    .Nav-right {
+        display: flex;
+        list-style: none;
+    }
+</style>
