fc39f231c3005bf8c8de1f0249ea725649d6d6fa
RubenErhardt <rubengamebattles@outlook.com>
2025-03-04 13:56:30 +0100

    rokertje

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -141,7 +141,7 @@
         transform: translateY(-50%);
         width: 50px;
         height: 50px;
-        background: url('imgs/rokertje') no-repeat;
+        background: url('imgs/rokertje.png') no-repeat;
         background-size: cover;
         animation: walk-header 20s linear infinite;
     }
